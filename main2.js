a41b1ae35d53946eb9e29af54ec42d9994a9b41d
Zeustakeshi <95726917+Zeustakeshi@users.noreply.github.com>
2022-01-09 16:20:16 +0700

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,78 @@
+var milisecond = 0 ;
+var second = 0 ;
+var munite = 0;
+var reset = false;
+var stop2 = false;
+var hiden = false;
+function start(){
+    reset = false
+    stop2 = false
+    var b = document.getElementById("start")
+    if ( b.innerText == "Bắt Đầu" || b.innerText == "Tiếp Tục"){
+        b.innerText = "Tạm Dừng"
+        if (stop2 == false){
+            setInterval (function (){
+                if (reset == true){
+                    document.getElementById("start").innerText = "Bắt Đầu"
+                    milisecond = 0;
+                    second = 0;
+                    munite = 0;
+                }
+                if (hiden == false){
+                    document.getElementById('screen2').innerHTML = munite+ " : " + second + " : " + milisecond
+                }else{
+                    document.getElementById('screen2').innerHTML = munite+ " : " + second 
+                }
+               
+                if (stop2 == false){
+                    ++milisecond;
+                    if (milisecond >99 ){
+                        milisecond = 0;
+                        ++second;
+                    }
+                    if (second > 59){
+                        second = 0;
+                        ++munite;
+                    }  
+                }
+               
+            },10)
+        }else{
+            document.getElementById('screen2').innerHTML = munite + " : " + second + " : " + milisecond
+        }
+        
+    }
+    else
+    {
+        b.innerText = "Tiếp Tục"
+        stop2 = true;   
+    }
+}
+
+
+function Reset (){
+    
+    reset = true;
+    milisecond = 0;
+    second = 0;
+    munite = 0;
+
+    document.getElementById('screen2').innerHTML = "0 : 0 : 0"
+}
+
+
+
+
+function hiden_mls (){
+    
+    var a= document.getElementById("hiden_mls");
+    if (a.innerText == "Ẩn Mili Giây"){
+        a.innerText = "Hiện Mili Giây"
+        hiden = true;
+    }
+    else{
+        a.innerText = "Ẩn Mili Giây"
+        hiden = false;
+    }
+   
+}
